c844f492f20a27a088df876b36b18530595709e9
ruhannn7 <ruhan.dogra7@gmail.com>
2026-01-21 12:02:46 +0530

    pehla

---
+++ script.js
@@ -0,0 +1,49 @@
+// LECTURE 1
+// const para = document.querySelector("p")
+// para.innerText="This is a New Paragraph."
+// para.style.color="green"
+// console.log(para)
+
+//LECTURE 2 AND 3 AND 4
+// console.log(document)
+// const para = document.getElementById("para")
+// para.innerText="hiiiiiii"
+
+// const para = document.getElementsByClassName("para")
+// console.log(para)
+// para[0].innerText="This is a New Paragraph."
+// para[1].style.color="green"
+// console.log(para)
+
+// const para = document.querySelectorAll(".para")
+// console.log(para)
+// para[0].innerText="This is a New Paragraph."
+// para[1].style.color="green"
+// console.log(para)
+
+// let arr=[23,134,13,14,45,67]
+// const newArr=arr.map((ele)=>ele) //it returns new array 
+// arr.forEach((ele)=>console.log(ele))// does not return new array
+
+
+// const container=document.querySelector(".container")
+// container.innerHTML=("<h1>Kaboom Kablow</h1>")
+// console.log(container)
+
+// LECTURE 5
+
+// const btn=document.querySelector("button")
+// btn.classList.add("btn")
+// // btn.classList.remove("btn")
+
+
+// // EVENT HANDLING
+// function message()
+// {
+//     alert("How are you")
+// }
+// // btn.addEventListener("mouseover",function(){alert("Hello")})
+// btn.addEventListener("click",message)
+// btn.removeEventListener("click",message)
+
+
