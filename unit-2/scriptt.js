848530ad5e15d9b25bb158f1555cfc494ef1d385
ruhannn7 <ruhan.dogra7@gmail.com>
2026-01-27 15:01:47 +0530

    feat: async and sync code

--- unit-2/scriptt.js
+++ unit-2/scriptt.js
@@ -1,14 +1,14 @@
 //lecture 1 
 console.log("first line")
-setTimeout(()=>{console.log("after 5 sec")},5000)
+setTimeout(()=>{console.log("after 1 sec")},1000)
+setTimeout(()=>{console.log("after 2 sec")},2000) //asynchronus behaviour 
+setTimeout(()=>{console.log("after 3 sec")},3000) //asynchronus behaviour 
+setTimeout(()=>{console.log("after 4 sec")},4000) //asynchronus behaviour 
 setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour 
-setTimeout(()=>{console.log("after 5 sec")},5000) //asynchronus behaviour  
+setTimeout(()=>{console.log("after 6 sec")},6000) //asynchronus behaviour 
+setTimeout(()=>{console.log("after 7 sec")},7000) //asynchronus behaviour 
+setTimeout(()=>{console.log("after 8 sec")},8000) //asynchronus behaviour 
+setTimeout(()=>{console.log("after 9 sec")},9000) //asynchronus behaviour 
+setTimeout(()=>{console.log("after 10 sec")},10000) //asynchronus behaviour 
+setTimeout(()=>{console.log("after 11 sec")},11000) //asynchronus behaviour  
 console.log("second line")
