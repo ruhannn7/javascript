ebcbb6d80f2d5e46431897ceb4b9579a63aa2134
ruhannn7 <ruhan.dogra7@gmail.com>
2026-04-22 12:08:09 +0530

    kaam

--- Unit3/my-app/src/App.jsx
+++ Unit3/my-app/src/App.jsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
-import Navbar from './component/Navbar';
-import Sample from './component/Sample';
+import React, { useState } from 'react'
+import './App.css'
+import Sample from './component/Sample'
+import Navbar from './component/Navbar'
+import HomePage from './component/HomePage'
+import Contact from './component/Contact'
 
 function App() {
-  const [menu, setMenu] = useState('Sports')
-  const [abc, setAbc] = useState('Entertainment')
+  const [menu, setMenu] = useState("Sports")
+  const [abc, setAbc] = useState("Entertainment")
+
   return (
     <>
-    <Navbar list={menu} />
-    <Navbar list={abc} />
-    <Sample />
+      <HomePage/>
+      <Navbar list={menu} />
+      <Navbar list={abc} />
+      <Sample />
+      <Contact/>
     </>
-    )
-  }
+  )
+}
 
-export default App;
+export default App
--- Unit3/my-app/src/component/HomePage.jsx
+++ Unit3/my-app/src/component/HomePage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function HomePage() {
-    const loggein = false
-  return (
-    loggein? <button>logout</button>:<button>Login</button>
-  
-  )
+  const loggedin=false;
+//   if(loggedin){
+//     return (
+//         <button>Logout</button>
+//     )
+//   }else{
+//     return (
+//         <button>Login</button>
+//     )
+//   }
+
+return(
+    loggedin?<button>Logout</button>: <button>Login</button>
+)
 }
 
 export default HomePage
